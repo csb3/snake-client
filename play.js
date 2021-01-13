0715a47e84861d4087fcf8e7dd6d3a23eb68e205
Cass Bredeson <c.s.bredeson@gmail.com>
2021-01-13 20:26:59 +0000

    created client module

--- play.js
+++ play.js
@@ -1,23 +1,6 @@
 /*PORT: `50542`
 HOST: `135.23.222.131`
 */
-
-const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50542
-  });
-
-  conn.setEncoding('utf8');
-  
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
