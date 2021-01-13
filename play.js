1e31090e10fb093484ca8fcf50da730dd159a000
Cass Bredeson <c.s.bredeson@gmail.com>
2021-01-13 20:56:55 +0000

    setup stdin with setupInput and handleUserInput

--- play.js
+++ play.js
@@ -4,3 +4,19 @@ HOST: `135.23.222.131`
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
