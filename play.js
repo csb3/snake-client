33bd9090ccf4a7e02305f02f2a8d8b3246b1a697
Cass Bredeson <c.s.bredeson@gmail.com>
2021-01-13 21:33:06 +0000

    implemented WASD movement

--- play.js
+++ play.js
@@ -2,21 +2,9 @@
 HOST: `135.23.222.131`
 */
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
-connect();
+const conn = connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-setupInput();
+setupInput(conn);
